a6ab21c31fb6be4f3f0ce120b7b978a766fc95d5
LukaszTylisz <lukasz.tylisz@gmail.com>
2024-08-07 19:44:03 +0200

    Start Adding Controllers

---
+++ src/Server/Controllers/CustomersController.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using BlazorShop.Application.Commands.CreateCustomer;
+using BlazorShop.Application.Commands.UpdateCustomer;
+using BlazorShop.Application.Exceptions;
+using BlazorShop.Application.Queries.GetCustomerById;
+using BlazorShop.Application.Queries.GetCustomers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Constants;
+using Shared.DTOs;
+
+namespace BlazorShop.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Produces("application/json")]
+public class CustomersController : Controller
+{
+    private readonly ISender _mediator;
+    private readonly IMapper _mapper;
+
+    public CustomersController(ISender mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<IEnumerable<CustomerDto>>> GetCustomers()
+    {
+        var customersDto = await _mediator.Send(new GetCustomersQuery());
+        return Ok(customersDto);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<CustomerDto>> GetCustomerById(Guid id)
+    {
+        var customerDto = await _mediator.Send(new GetCustomerByIdQuery(id));
+
+        return customerDto == null
+            ? (ActionResult<CustomerDto>)NotFound()
+            : (ActionResult<CustomerDto>)Ok(customerDto);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> CreateCustomer(CreateCustomerDto customerDto)
+    {
+        var customerCmd = new CreateCustomerCommand(Guid.NewGuid(), customerDto.Name);
+        await _mediator.Send(customerDto);
+
+        return CreatedAtAction(nameof(GetCustomerById), ControllerNameConstants.Customers, new { id = customerCmd.Id }, customerCmd);
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> UpdateCustomer(Guid id, UpdateCustomerDto customerDto)
+    {
+        if (id != customerDto.Id)
+        {
+            return BadRequest();
+        }
+
+        var customerCmd = new UpdateCustomerCommand(customerDto.Id, customerDto.Name);
+
+        try
+        {
+            await _mediator.Send(customerCmd);
+        }
+        catch (NotFoundException)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+}
---
+++ src/Server/Controllers/OrdersController.cs
@@ -0,0 +1,92 @@
+﻿using AutoMapper;
+using BlazorShop.Application.Commands.ChangeOrderStatus;
+using BlazorShop.Application.Commands.CreateOrder;
+using BlazorShop.Application.Exceptions;
+using BlazorShop.Application.Queries.GetOrderById;
+using BlazorShop.Application.Queries.GetOrders;
+using BlazorShop.Application.Queries.GetProductById;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Constants;
+using Shared.DTOs;
+using Shared.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Produces("application/json")]
+public class OrdersController : Controller
+{
+    private readonly ISender _mediator;
+    private readonly IMapper _mapper;
+
+    public OrdersController(ISender mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<IEnumerable<OrderDto>>> GetOrders()
+    {
+        var ordersDto = await _mediator.Send(new GetOrdersQuery());
+        return Ok(ordersDto);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<OrderDto>> GetOrderById(Guid id)
+    {
+        var orderDto = await _mediator.Send(new GetOrderByIdQuery(id));
+        if (orderDto is null)
+            return NotFound();
+
+        return Ok(orderDto);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> CreateOrder(CreateOrderDto orderDto)
+    {
+        var orderItemsCmd = new List<CreateOrderItemCommand>();
+
+        foreach (var orderItemDto in orderDto.OrderItems)
+        {
+            var productDto = await _mediator.Send(new GetProductByIdQuery(orderItemDto.ProductId));
+            orderItemsCmd.Add(new CreateOrderItemCommand(orderItemDto.ProductId, orderItemDto.Quantity, productDto.Price));
+        }
+
+        var orderCmd = new CreateOrderCommand(Guid.NewGuid(), orderDto.CustomerId.Value, orderDto.City, orderDto.Street, orderItemsCmd);
+        await _mediator.Send(orderCmd);
+
+        return CreatedAtAction(nameof(GetOrderById), ControllerNameConstants.Orders, new { id = orderCmd.Id }, orderCmd);
+    }
+
+    [HttpPut("{id}/Status")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> ChangeOrderStatus(Guid id, [FromBody][EnumDataType(typeof(OrderStatus))] OrderStatus orderStatus)
+    {
+        try
+        {
+            await _mediator.Send(new ChangeOrderStatusCommand(id, orderStatus));
+        }
+        catch (NotFoundException)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+}
