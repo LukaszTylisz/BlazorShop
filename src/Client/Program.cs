61fb58a8f296bb30849e14a4650eb147ef16ce03
LukaszTylisz <lukasz.tylisz@gmail.com>
2024-08-12 12:06:55 +0200

    added rest of pages

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,11 +1,26 @@
+using Blazored.LocalStorage;
+using Blazored.Toast;
+using Client;
+using Client.HttpClients;
+using Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient<ShopHttpClient>(c =>
+{
+    c.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+});
+
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredToast();
+
+builder.Services.AddSingleton<AppStateService>();
+builder.Services.AddScoped<CartService>();
 
 await builder.Build().RunAsync();
